be0448548775f13d7b1e9d873fd8cb0481b571cf
luka-gril <luka.gril@student.um.si>
2017-12-05 17:43:57 +0100

    Popravek animacije

--- Rpo igra 2/Assets/Modeli/Janja/Boti/BotPremikanje.cs
+++ Rpo igra 2/Assets/Modeli/Janja/Boti/BotPremikanje.cs
@@ -7,15 +7,24 @@ public class BotPremikanje : MonoBehaviour {
 
 	private NavMeshAgent NavMeshAgentKomponenta;
 	private Transform mPlayerTransform;
+	private Animator mBotAnimator; // komponenta za animacije
 
 	void Start () {
-		NavMeshAgentKomponenta = GetComponent<NavMeshAgent> ();
+		NavMeshAgentKomponenta = GetComponent<NavMeshAgent> (); 
 		mPlayerTransform = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+		mBotAnimator = GetComponent<Animator> ();
 	}
 
 	void Update () {
 		Vector3 playerPosition = mPlayerTransform.position;
 		NavMeshAgentKomponenta.SetDestination (playerPosition);
+
+		if (NavMeshAgentKomponenta.velocity == Vector3.zero) { // če bot stoji na mestu
+			mBotAnimator.SetBool ("Running", false); // nastavimo running spremenljivko na 0, kar menja animacijo na mirovanje
+		} else {
+			mBotAnimator.SetBool ("Running", true); // drugače nastavimo running na true kar menja animacijo na tekanje
+		}
+
 	}
 
 
